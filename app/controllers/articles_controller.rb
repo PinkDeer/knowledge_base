3129d4cb8023b6e0cffd895deef4f0d7bbc332e6
PinkDeer <bunman@mail.ru>
2017-11-19 00:50:32 +0300

    Update actions in contoller, add views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
   def index
+    @articles = Article.all.order("created_at DESC")
   end
 
   def show
@@ -10,7 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(post_params)
+    @article = Article.new(article_params)
     if @article.save
 			redirect_to @article
 		else
@@ -22,17 +26,25 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+end
   end
 
   def destroy
+    @article.destroy
+		redirect_to root_path
   end
 
   private
 
-    def find_post
+    def find_article
+      @article = Article.find(params[:id])
     end
 
-    def post_params
+    def article_params
       params.require(:article).permit(:title, :content)
     end
 
