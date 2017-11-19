10f834db0fefe78d0ac15681205d0340fa3c3d78
PinkDeer <bunman@mail.ru>
2017-11-19 14:53:58 +0300

    Add and setup Trix gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119095142) do
+ActiveRecord::Schema.define(version: 20171119112859) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20171119095142) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
